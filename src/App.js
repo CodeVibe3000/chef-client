0c08b151bf291912609a81046994be7f5630bc22
CodeVibe3000 <dacoolgi2@gmail.com>
2020-07-25 15:10:08 -0700

    home

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./Home.css"
 function App() {
   return (
     <div className="App">
-      <iframe title="home" style={{border:"none", width:"100%", height:"100%"}} src="https://chefapphomepage.netlify.app"></iframe>  
+      <iframe title="home" style={{border:"none", width:"100%", height:"100vh"}} src="https://chefapphomepage.netlify.app"></iframe>  
     </div>
   );
 }
